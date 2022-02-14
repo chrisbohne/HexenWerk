3808493f1a5610340182168a7f8dc5c76329fc0a
chrisbohne <christoph.bohne90@gmail.com>
2022-02-14 12:57:15 +0100

    Add image,svg and css loader to webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css';
+import Image from './test.svg';
+
 export const App = () => {
-  return <h1>HexFinder</h1>;
+  return (
+    <>
+      <h1>HexFinder</h1>
+      <img src={Image} width="300" height="200" />
+    </>
+  );
 };
