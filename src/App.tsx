1d202d804d6fcf588ccacfe3c55267e1ae686719
chrisbohne <christoph.bohne90@gmail.com>
2022-02-14 14:14:12 +0100

    Integrate eslint

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ export const App = () => {
   return (
     <>
       <h1>HexFinder</h1>
-      <img src={Image} width="300" height="200" />
-      <p>{process.env.TEST}</p>
+      <img src={Image} alt="react logo" width="300" height="200" />
     </>
   );
 };
