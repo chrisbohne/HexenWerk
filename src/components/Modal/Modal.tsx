bfed051bf9f28b86d76259f6b5b20f830aae1665
chrisbohne <christoph.bohne90@gmail.com>
2022-02-27 13:30:58 +0100

    Update modal to close on button and outside click

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,6 @@
+/* eslint jsx-a11y/no-static-element-interactions: 0 */
+/* eslint jsx-a11y/click-events-have-key-events: 0 */
+
 import { FC, MutableRefObject, ReactNode, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.module.scss';
@@ -28,11 +31,13 @@ const Modal: FC<IProps> = ({ children, close }) => {
   }, []);
 
   return createPortal(
-    <div className={styles.modal}>
-      <button className={styles.button} onClick={close}>
-        close
-      </button>
-      {children}
+    <div className={styles.modal__container} onClick={close}>
+      <div onClick={(e) => e.stopPropagation()} className={styles.modal}>
+        <button className={styles.button} onClick={close}>
+          X
+        </button>
+        {children}
+      </div>
     </div>,
     elRef.current
   );
