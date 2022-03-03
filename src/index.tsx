3b2442e9eb9831706fb263e267450064c03df9d9
chrisbohne <christoph.bohne90@gmail.com>
2022-03-03 20:11:53 +0100

    Add redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './store';
 
 ReactDom.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
   document.getElementById('root')
 );
